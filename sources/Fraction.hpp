9594155f6a0547d9f84a3fec73b67f38bcc616ab
davidstern135 <122543674+davidstern135@users.noreply.github.com>
2023-05-08 17:18:35 +0300

    Update Fraction.hpp

--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -15,7 +15,6 @@ namespace ariel{
     public:
         Fraction();
         Fraction(int numerator,int denominator);
-//        Fraction(double doubleNum);
         Fraction(float floatNum);
 
         int getNumerator() const;
@@ -27,8 +26,6 @@ namespace ariel{
         friend Fraction operator+(const Fraction& fraction,float float_num);
         friend Fraction operator+(float float_num, const Fraction& fraction);
 
-//        friend Fraction operator+(const double double_num, const Fraction &fraction);
-//        friend Fraction operator+(const Fraction &fraction, const double double_num);
 
         Fraction operator-(const Fraction& fraction) const;
         friend Fraction operator-(const Fraction& fraction,float float_num);
